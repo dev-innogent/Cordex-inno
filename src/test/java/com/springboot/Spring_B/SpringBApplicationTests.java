baca4dd5334944380deaca47d50d66d02c5c8e3a
dev-innogent <dev@innogent.in>
2025-06-06 16:38:27 +0530

    Removed unwanted code.

--- src/test/java/com/springboot/Spring_B/SpringBApplicationTests.java
+++ src/test/java/com/springboot/Spring_B/SpringBApplicationTests.java
@@ -1,6 +1,5 @@
 package com.springboot.Spring_B;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 /**
@@ -8,9 +7,5 @@ import org.springframework.boot.test.context.SpringBootTest;
  */
 @SpringBootTest
 class SpringBApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
